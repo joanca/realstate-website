1c1d9c448158f55a669c98d8dbf119832dc8b5e8
joanca <jcaravenae@gmail.com>
2026-03-02 22:58:18 +0100

    add ?dev param for localhost esm.sh imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'https://esm.sh/react@18';
 import { createRoot } from 'https://esm.sh/react-dom@18/client';
-import App from 'https://esm.sh/gh/joanca/realstate-website@main/src/App.jsx?jsx';
+
+const isDev = ['localhost', '127.0.0.1'].includes(window.location.hostname);
+const devParam = isDev ? '&dev' : '';
+
+const App = (await import(`https://esm.sh/gh/joanca/realstate-website@main/src/App.jsx?jsx${devParam}`)).default;
 
 const root = createRoot(document.getElementById('emily-realestate'));
 root.render(React.createElement(App));

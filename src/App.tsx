241f527b1ad61b18dd7a30be7fe4bc8155e06489
joanca <jcaravenae@gmail.com>
2026-03-04 16:27:56 +0100

    refactor: inline app root into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-import { AppRoot } from './modules/app/AppRoot'
+import { useEffect } from 'react'
+import { ExperienceBar } from './components/ExperienceBar'
+import { HeroSection } from './components/HeroSection'
+import { TestimonialsSection } from './components/TestimonialsSection'
+import { normalizeEmbeddedDom } from './modules/embed/normalizeEmbeddedDom'
+import { experienceContent, heroContent, testimonials, testimonialsContent } from './modules/app/appContent'
 
 export default function App() {
-  return <AppRoot />
+  useEffect(() => {
+    normalizeEmbeddedDom()
+  }, [])
+
+  return (
+    <div className="bg-white min-h-screen">
+      <HeroSection imageUrl={heroContent.imageUrl} arrowUrl={heroContent.arrowUrl} ctaBackgroundUrl={heroContent.ctaBackgroundUrl} />
+      <ExperienceBar years={experienceContent.years} label={experienceContent.label} />
+      <TestimonialsSection
+        testimonials={testimonials}
+        starsImageUrl={testimonialsContent.starsImageUrl}
+        allTestimonialsHref={testimonialsContent.allTestimonialsHref}
+      />
+    </div>
+  )
 }

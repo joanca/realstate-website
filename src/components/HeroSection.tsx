f0726dae6b6e59a2acd3c6e2e11f17edba6eaa63
joanca <jcaravenae@gmail.com>
2026-03-31 21:51:44 +0200

    fix: separator line now allows the image on hero to be visible on top of it

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ interface HeroSectionProps {
 export function HeroSection({ imageUrl, arrowUrl, ctaBackgroundUrl }: HeroSectionProps) {
   return (
     <section className="hero-surface relative">
-      <div className="relative mx-auto px-4 lg:px-20 py-[2.2rem] max-w-[1440px]">
+      <div className="relative mx-auto px-4 lg:px-20 pt-[2.2rem] max-w-[1440px]">
         <div className="flex flex-col lg:flex-row lg:items-start">
           <div className="w-full lg:w-2/3 lg:pr-16">
             <div className="flex flex-row lg:flex-col items-center lg:items-start">
